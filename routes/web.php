f2420a9aced124ebca9c5b4836941669f9931d4f
nabilana <nabilaapriliyanti807@gmail.com>
2022-01-26 09:57:58 +0700

    laravel

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
+    return view('home', [
+        "title" => "Home"
+    ]);
+});
+
+Route::get('/about', function () {
     return view('about',[
-        "nama"=> "Nabila Nur Apriliyanti",
-        "email" => "nabilaapriliyanti807",
+        "title" => "About",
+        "nama" => "Nabila Nur Apriliyanti",
+        "email" => "Nabilaapriliyanti@gmail.com",
         "gambar" => "Foto Almet.jpg"
     ]);
+});
+Route::get('/gallery', function () {
+        return view('gallery', [
+            "title" => "Gallery"
+        ]);
 });
 
